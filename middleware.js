31a01eab004c456bde0645bc75d6f7a18d9b159f
Dedcode77 <salifciss222@gmail.com>
2025-05-22 12:24:44 +0200

    Add Vercel config to fix dependency conflict

--- middleware.js
+++ middleware.js
@@ -1,26 +1,22 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-// 1. Définissez les routes à protéger
+// Définis ici les routes à protéger
 const isProtectedRoute = createRouteMatcher([
-  '/dashboard(.*)',       // Protège tout sous /dashboard
-  '/profile(.*)',        // Protège tout sous /profile
-  '/admin(.*)',          // Protège tout sous /admin
+  '/dashboard(.*)',
+  '/profile(.*)',
+  '/admin(.*)',
 ]);
 
 export default clerkMiddleware((auth, req) => {
-  // 2. Applique la protection uniquement aux routes définies
   if (isProtectedRoute(req)) {
     auth().protect();
   }
-
-  // 3. Pour les routes publiques, aucune action n'est nécessaire
 });
 
 export const config = {
-  // 4. Configuration des routes à analyser par le middleware
   matcher: [
-    "/((?!.*\\..*|_next).*)",  // Exclut les fichiers statiques
-    "/",                        // Inclut la page d'accueil
-    "/(api|trpc)(.*)",          // Inclut les routes API
+    "/((?!.*\\..*|_next).*)",
+    "/",
+    "/(api|trpc)(.*)",
   ],
 };
